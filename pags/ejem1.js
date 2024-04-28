7c8e8e6e284413a325f09b32b32f6a4a34ba6f5b
mnaspiazu <70596442+mnaspiazu@users.noreply.github.com>
2024-04-27 21:12:33 -0400

    Add files via upload

---
+++ pags/ejem1.js
@@ -0,0 +1,9 @@
+document.write("llamado desde otro archivo");
+var nom,a,b,s;
+nom=prompt("Hola ingrese su nombre:....");
+alert(" Hola "+nom);
+document.write(" Hola bievenvido "+nom);
+a=parseInt(prompt(" a: "));
+b=parseInt(prompt(" b: "));
+s=a+b;
+alert("LA suma es: "+s);
